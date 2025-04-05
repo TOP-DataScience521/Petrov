f35538d5c53a85a2aef0bab5dfc2474fe55d4cad
Gennadiy <renardes@gmail.com>
2025-04-05 22:33:31 +0500

    2025.03.09: оформление

--- 2025.03.09/3.py
+++ 2025.03.09/3.py
@@ -5,5 +5,7 @@ interval_min = number % 60
 
 print(f'{number} мин. - это {interval_hour} час. {interval_min} мин.')
 
+
 # Введите количество минут: 500
 # 500 мин. - это 8 час. 20 мин.
+
--- 2025.03.09/5.py
+++ 2025.03.09/5.py
@@ -1,10 +1,11 @@
 number1 = str(input('Введите целое число миль: '))
 number2 = str(input('Введите дробное число миль: '))
 
-mile_all = round(float(number1+'.'+number2), 2)
+mile_all = round(float(number1 + '.' + number2), 2)
 km_all = round(mile_all*1.61, 1)
 
 print(f'{mile_all} миль = {km_all} км.')
+
 
 # Введите целое число миль: 30
 # Введите дробное число миль: 55555
