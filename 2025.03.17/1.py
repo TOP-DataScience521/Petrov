c1371b4cc378808fe1df1aee5693a8f94a432ec8
Gennadiy <renardes@gmail.com>
2025-04-09 22:38:17 +0500

    2025.03.17: проверено

--- 2025.03.17/1.py
+++ 2025.03.17/1.py
@@ -3,23 +3,31 @@ num2 = float(input('Введите второе число: '))
 num3 = float(input('Введите третье число: '))
 
 if num1 > 0:
-     result1 = num1
+    # УДАЛИТЬ: избыточная переменная
+    result1 = num1
 else: 
     result1 = 0
     
 if num2 > 0:
-     result2 = num2
+    # УДАЛИТЬ: избыточная переменная
+    result2 = num2
 else: 
     result2 = 0
     
 if num3 > 0:
-     result3 = num3
+    # УДАЛИТЬ: избыточная переменная
+    result3 = num3
 else: 
     result3 = 0
 
 print(result1+result2+result3)
 
+
 # Введите первое число: 15
 # Введите второе число: -100
 # Введите третье число: 3.73295
 # 18.73295
+
+
+# ИТОГ: 2/3
+
